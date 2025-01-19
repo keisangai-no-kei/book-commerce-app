5571fd20a133259fcf010b76116887a41a2ada0b
keisangai-no-kei <as.kosuke.is@icloud.com>
2025-01-19 17:51:08 +0900

    mod11

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
-
 export async function POST(request: Request) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
   const { title, price, bookId, userId } = await request.json();
   try {
     const session = await stripe.checkout.sessions.create({
