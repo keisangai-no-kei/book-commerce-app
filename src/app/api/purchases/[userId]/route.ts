7f50a71bbf898d3c3d43a141821ff773338a6ae8
keisangai-no-kei <as.kosuke.is@icloud.com>
2025-01-19 17:22:12 +0900

    mod5

--- src/app/api/purchases/[userId]/route.ts
+++ src/app/api/purchases/[userId]/route.ts
@@ -3,9 +3,9 @@ import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
-  { params }: { params: { userId: string } }
+  { params }: { params: Promise<{ userId: string }> }
 ) {
-  const userId = params.userId;
+  const userId = (await params).userId;
   try {
     const purchases = await prisma.purchase.findMany({
       where: { userId: userId },
